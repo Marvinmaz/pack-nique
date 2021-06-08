68fa200470980ea320076c7a4643de7690088456
Sébastien Michaut <sebmichaut00@gmail.com>
2021-06-08 17:41:06 +0200

    CRUD du pack en cours-update

--- src/Controller/PackController.php
+++ src/Controller/PackController.php
@@ -2,13 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Pack;
+use App\Form\PackType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class PackController extends AbstractController{
     /**
      * @Route("/CreatePack", name="pack")
      */
     public function newPack(Request $request): Response{
-        $
+        $pack = new Pack();
+
+        $form = $this->createForm(PackType::class, $pack);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($pack);
+            $em->flush();
+
+            return $this->redirectToRoute("base");
+        }
+
+        return $this->render('pack/createPack.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
