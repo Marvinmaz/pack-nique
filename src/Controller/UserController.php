5c402e21bbdd808d35c61077061952a0fab588b1
Marvin <marvinmazel95@gmail.com>
2021-06-08 15:54:53 +0200

    account

---
+++ src/Controller/UserController.php
@@ -0,0 +1,42 @@
+<?php 
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController{
+
+    /**
+     * @Route("/CreateAccount", name="account")
+     */
+    public function createAccount(Request $request): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('account');
+        }
+        return $this->render("./account.html.twig", [
+            'form' => $form->createView()
+        ]);
+    }
+
+
+
+
+
+}
---
+++ src/Form/UserType.php
@@ -0,0 +1,39 @@
+<?php 
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName', TextType::class, ['label' => 'Prénom'])
+            ->add('name', TextType::class, ['label' => 'Nom'])
+            ->add('age', NumberType::class, ['label' => 'Age'])
+            ->add('mail', EmailType::class, ['label' => 'Adresse Email'])
+            ->add('password', PasswordType::class, ['label' => 'Votre Mot de passe'])
+            ->add('address', TextType::class, ['label' => 'Votre adresse de residence'])
+            ->add('postalCode', NumberType::class, ['label' => 'Votre code postale'])
+            ->add('country', TextType::class, ['label' => 'Votre ville'])
+            ->add('tel', NumberType::class, ['label' => 'Téléphone'])
+            ->add('pics', UrlType::class, ['label' => 'Ajouter une photo de profil']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class
+        ]);
+    }
+
+}
