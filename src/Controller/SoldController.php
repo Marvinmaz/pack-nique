8e7e44747c5f86272e35c1abaa2e9826f9d705eb
Sébastien Michaut <sebmichaut00@gmail.com>
2021-06-14 17:41:16 +0200

    prix total a afficher

--- src/Controller/SoldController.php
+++ src/Controller/SoldController.php
@@ -5,18 +5,27 @@ namespace App\Controller;
 use App\Entity\Sold;
 use App\Form\SoldType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class SoldController extends AbstractController{
+
     /**
      * @Route("/sold", name="sold")
      */
-    public function createSold(): Response {
+    public function createSold(SessionInterface $session): Response {
         $sold = new Sold();
         $form = $this->createForm(SoldType::class, $sold);
-
+        $basket = $session->get("basket");
+        $totalPrice = $session->get("totalPrice");
+        dump($totalPrice);
+        // foreach ($basket as $id => $quantity) {
+            
+        // }
         return $this-> render('sold/sold.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+
         ]);
     }
 }
--- src/Form/SoldType.php
+++ src/Form/SoldType.php
@@ -8,23 +8,16 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+
 class SoldType extends AbstractType{
     public function buildForm(FormBuilderInterface $builder, array $options){
        
         $builder
-            ->add('address', TextType::class, ['label' => 'Adresse'])
-            ->add('postalcode', TextType::class, ['label' => 'Code Postal'])
-            ->add('firstname', TextType::class, ['label' => 'Prénom'])
-            ->add('name', TextType::class, ['label' => 'Nom'])
-            ->add('age', TextType::class, ['label' => 'Age'])
-            ->add('tel', TextType::class, ['label' => 'Téléphone'])
-            ->add('country', TextType::class, ['label' => 'ville'])
-            ->add('code', Sold::class, ['label' => 'Code promo',
-                                        'class' => TextType::class]);
+            // ->add('user', UserInfoType::class, ["label" => "Coordonnées"])
+            ->add('code', TextType::class, ['label' => 'Code promo']);
     }
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver){
         $resolver->setDefaults([
             'data_class' => Sold::class
         ]);
